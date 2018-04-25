d61108156a6b94d5bda6d5547f12e14de080646a
Manolis Savva <manolis.savva@gmail.com>
2018-04-25 09:53:02 -0700

    update getdata.sh to create metadata symlinks

--- getdata.sh
+++ getdata.sh
@@ -6,3 +6,7 @@ METADATA_DIR='./metadata'
 [ -e $METADATA_DIR ] && rm -rf $METADATA_DIR
 
 git clone https://github.com/smartscenes/sstk-metadata.git --branch $METADATA_VER --single-branch $METADATA_DIR
+
+for l in 'labels' 'matterport' 'shapenet' 'suncg'; do
+  ln -s ../../../metadata/data/$l server/static/data/$l
+done
