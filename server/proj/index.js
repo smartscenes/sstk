1c7492da31ecabbbf6948f27d3fcf460c12b06c5
angelxuanchang <angelx@gmail.com>
2018-04-25 21:37:54 -0700

    Update index.js

--- server/proj/index.js
+++ server/proj/index.js
@@ -2,7 +2,6 @@
 
 module.exports = [
   { name: 'scannet', app: require('./scannet'), mountpath: '/scans' },
-  { name: 'shapenet', app: require('./shapenet'), mountpath: '/' },
   { name: 'sim', app: require('./sim'), mountpath: '/' },
   // { name: 'projectname', app: require('./projectname'), mountpath: '/' },
 ];
