1081969fa04380a51b54cb7ff65995a4ab203485
Angel Chang <angelx@gmail.com>
2024-03-11 08:54:46 -0700

    Comment out subproject

--- server/proj/index.js
+++ server/proj/index.js
@@ -6,6 +6,6 @@ module.exports = [
   { name: 'multiscan', app: require('./multiscan'), mountpath: '/' },
   { name: 'sim', app: require('./sim'), mountpath: '/' },
   { name: 'rlsd', app: require('./rlsd'), mountpath: '/' },
-  { name: 'rlsd-scene-manager-client', app: require('./rlsd-scene-manager-client'), mountpath: '/scene-manager' }
+  // { name: 'rlsd-scene-manager-client', app: require('./rlsd-scene-manager-client'), mountpath: '/scene-manager' }
   // { name: 'projectname', app: require('./projectname'), mountpath: '/' },
 ];
